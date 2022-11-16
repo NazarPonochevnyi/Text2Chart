d97027d63541828517e55ca199b24774e4c310a8
Nazar Ponochevnyi <nazar.ponochevnyi@gmail.com>
2022-11-16 12:45:01 -0500

    Update gptj.py

--- gptj.py
+++ gptj.py
@@ -162,7 +162,7 @@ config = transformers.GPTJConfig.from_pretrained("EleutherAI/gpt-j-6B")
 tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 
 
-gpt = GPTJForCausalLM.from_pretrained("hivemind/gpt-j-6B-8bit", low_cpu_mem_usage=True)
+gpt = GPTJForCausalLM.from_pretrained("hivemind/gpt-j-6B-8bit")
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 gpt.to(device)
